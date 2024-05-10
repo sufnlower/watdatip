c390507b2785da4daef16278ee1285e69f40de9a
= <ds_walters@hotmail.com>
2024-05-10 11:56:30 +1000

    broke into funcs and added redirect check

--- watdatip.py
+++ watdatip.py
@@ -1,8 +1,46 @@
 #!/usr/bin/python3
-import sys
 import argparse
 import subprocess
 import re
+
+def sslsubjectcheck(ip):
+    hostname = ""
+    command = f"/usr/bin/curl --insecure -vvI https://{ip} 2>&1 | /usr/bin/awk 'BEGIN {{ cert=0 }} /^\* SSL connection/ {{ cert=1 }} /^\*/ {{ if (cert) print }}'"
+    result = subprocess.run(command,capture_output=True, text=True, shell=True, encoding='utf-8', errors='ignore')
+    cn_pattern =  r"CN=([^;\n]+)"
+    match = re.search(cn_pattern, result.stdout)    
+    if match:
+        hostname = match.group(1)
+    return hostname
+    
+def redirectcheck(ip):      
+    print("doing redirect check")
+    command = f"/usr/bin/curl --insecure -I https://{ip} | grep location"
+    result = subprocess.run(command,capture_output=True, text=True, shell=True, encoding='utf-8', errors='ignore')
+    
+    pattern = r"(?<=http:\/\/)[^\/\?]+"
+    match = re.search(pattern, result.stdout)
+    if match:
+        hostname = match.group()
+        return hostname
+    pattern = r"(?<=https:\/\/)[^\/\?]+"
+    match = re.search(pattern, result.stdout)
+    if match:
+        hostname = match.group()
+        return hostname
+    else:
+        return None
+        
+def nslookupcheck(ip,hostname):
+    command = f"/usr/bin/nslookup {hostname}"
+    result = subprocess.run(command,capture_output=True, text=True, shell=True, encoding='utf-8', errors='ignore')
+    address_pattern = r"Address: ([\d\.:a-f]+)"
+    addresses = re.findall(address_pattern, result.stdout)
+    for address in addresses:
+        if address == ip:
+            return (f"{address} {hostname}")
+    return None
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -10,37 +48,38 @@ def main():
     parser.add_argument('-f','--file', help="Input file, one line at a time")
     args = parser.parse_args()
 
-    command = f"/usr/bin/curl --insecure -vvI https://{args.ip} 2>&1 | /usr/bin/awk 'BEGIN {{ cert=0 }} /^\* SSL connection/ {{ cert=1 }} /^\*/ {{ if (cert) print }}'"
+    ips = []
+    results = []
+
+    if args.ip != None:
+        ips.append(args.ip)
+    elif args.file != None:
+        with open(args.file, 'r') as file:
+            lines = file.readlines()
+            lines = [line.strip() for line in lines]
+        ips = lines
     
-    print(command)
-    result = subprocess.run(command,capture_output=True, text=True, shell=True, encoding='utf-8', errors='ignore')
-
-    output = result.stdout
-
-    cn_pattern =  r"CN=([^;\n]+)"
-    match = re.search(cn_pattern, output)
-
-    if match:
-        hostname = match.group(1)
+     
+    for ip in ips:
+        print("doing ssl subject check")
+        hostname = sslsubjectcheck(ip)
         print(hostname)
 
-        command = f"/usr/bin/nslookup {hostname}"
-        result = subprocess.run(command,capture_output=True, text=True, shell=True, encoding='utf-8', errors='ignore')
-        print(result.stdout)
-        # Regular expression to extract Address
-        address_pattern = r"Address: ([\d\.:a-f]+)"
+        ns = nslookupcheck(ip,hostname)
+        if ns != None:
+            results.append(ns)
+            continue
 
-        # Find all matches in the string
-        addresses = re.findall(address_pattern, result.stdout)
+        hostname = redirectcheck(ip)
+        ns = nslookupcheck(ip,hostname)
+        if ns != None:
+            results.append(ns)
+            continue
+    
+    print(results)       
 
-        # Print each found address
-        for address in addresses:
-            if address == args.ip:
-                print("match found")
-                print(f"{hostname} {address}")
+
+
 
 if __name__ == "__main__":
     main()
-
-#curl --insecure -vvI https://93.184.215.14 2>&1 | awk 'BEGIN { cert=0 } /^\* SSL connection/ { cert=1 } /^\*/ { if (cert) print }'| grep subject
-
